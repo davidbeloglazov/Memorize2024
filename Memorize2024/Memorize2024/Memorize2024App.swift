fd4ee14f91cf033684fd007e620dd0be2608b0b6
Chris Beloglazov <vb9129@princeton.edu>
2024-07-29 12:45:33 -0400

    lecture 1

---
+++ Memorize2024/Memorize2024/Memorize2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2024App.swift
+//  Memorize2024
+//
+//  Created by Chris Beloglazov on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
