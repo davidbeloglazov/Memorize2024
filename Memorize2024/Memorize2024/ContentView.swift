51aebd5b387cb0b4a7987f244328a0bcce175cf6
Chris Beloglazov <vb9129@princeton.edu>
2024-07-29 13:17:21 -0400

    2.1 Toggleable isFaceUp

--- Memorize2024/Memorize2024/ContentView.swift
+++ Memorize2024/Memorize2024/ContentView.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 
-// a View is a rectangular area on screen that draws something
+// a View is a rectangular area on screen that draws something. Views are immutable
+// Swift is a very strongly typed language, use type inference
 
 struct ContentView: View { // protocol oriented programming, behaves like a View
     var body: some View { // of type some View, a computed property
@@ -23,21 +24,25 @@ struct ContentView: View { // protocol oriented programming, behaves like a View
     }
 }
 
-struct CardView: View{
-    var isFaceUp: Bool = false
-    var body: some View{
-        ZStack {
+struct CardView: View {
+    @State var isFaceUp = false // without @State we have an error
+    var body: some View { // only lets in View because it's read-only
+        ZStack { // trailing closure syntax
+            let base = RoundedRectangle(cornerRadius: 12) // type inference
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 10)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 20)
+                base.fill()
             }
             
-        } // the stuff in curly brackets is a function
+        }.onTapGesture {
+//            print("tapped")
+//            isFaceUp = !isFaceUp
+            isFaceUp.toggle()
+        }
+        // the stuff in curly brackets is a function
         // VStack(content: {Image() Text()})
     }
 }
