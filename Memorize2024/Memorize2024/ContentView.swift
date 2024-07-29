91625fd2696c18176fa35fa74753e3d68b488e46
Chris Beloglazov <vb9129@princeton.edu>
2024-07-29 12:47:00 -0400

    lec 1.1

--- Memorize2024/Memorize2024/ContentView.swift
+++ Memorize2024/Memorize2024/ContentView.swift
@@ -7,17 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+// a View is a rectangular area on screen that draws something
+
+struct ContentView: View { // protocol oriented programming, behaves like a View
+    var body: some View { // of type some View, a computed property
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        
+        .foregroundColor(.orange)
         .padding()
     }
 }
+
+struct CardView: View{
+    var isFaceUp: Bool = false
+    var body: some View{
+        ZStack {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 10)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 20)
+            }
+            
+        } // the stuff in curly brackets is a function
+        // VStack(content: {Image() Text()})
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
